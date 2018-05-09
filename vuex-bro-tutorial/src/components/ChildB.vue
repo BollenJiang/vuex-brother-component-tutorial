8174807427343747c2504650d3118a7043a62b04
bluesbonew <bluesbonewong@gmail.com>
2018-05-09 10:38:30 +0800

    完善逻辑

---
+++ vuex-bro-tutorial/src/components/ChildB.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="childB">
+    <h1>我是B组件</h1>
+    <button @click="transform">点我让A组件接收到数据</button>
+    <p>因为你点了A，所以我的数字发生了变化：{{AMessage}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        BMessage: 'Hello，B组件，我是A组件'
+      }
+    },
+    computed: {
+      AMessage() {
+        return this.$store.state.AMsg
+      }
+    },
+    methods: {
+      transform() {
+        this.$store.commit('receiveBMsg', {
+          BMsg: this.BMessage
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+  div#childB {
+    border: 1px solid green;
+  }
+</style>
